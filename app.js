1e413f2ec1536a3708edaf0e75da181d431f8b38
Lucas_ <lm742611149@gmail.com>
2020-03-16 15:48:55 +0400

    try reset

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 // app.js
 function hello() {
   alert("hello world git1");
+  alert("test");
+}
+
+function helloworld() {
+  alert("hi");
+}
+
+function secondFunc() {
+  alert("second function");
 }
